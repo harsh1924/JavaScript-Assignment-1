5b66e70b34ef2b33ea605d07bf0b35c7f72571a4
harsh1924 <harshrpanwar@gmail.com>
2023-07-29 20:49:07 +0530

    Sixth Question

---
+++ 6.js
@@ -0,0 +1,12 @@
+let name = "Harsh";
+let email = "hsdqsd@gmail.com";
+let age = "22";
+if (typeof name != 'string' ) {
+    console.log("Name should be a string");
+}
+if (typeof email != 'string' ) {
+    console.log("Email should be a string");
+}
+if (typeof age != 'number') {
+    console.log("Age should be a number");
+}
