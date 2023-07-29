3fd2af072e49d859857f0200ef73b815e17b7bda
harsh1924 <harshrpanwar@gmail.com>
2023-07-29 20:46:43 +0530

    First Question

---
+++ 1.js
@@ -0,0 +1,4 @@
+let totalValue = 2000;
+let discountPercentage = 20;
+let finalValue = totalValue - (totalValue * discountPercentage) / 100;
+console.log("The Final Price after discount is Rs." + finalValue);
